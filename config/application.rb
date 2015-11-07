f72a7679b484e99d726dd3ad4dd92460e9d1dffb
Adam Ki Jensen <adajensen@gmail.com>
2015-11-07 13:20:00 -0700

    first try integrating email to a rails app

---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Emailery
+  class Application < Rails::Application
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.mandrillapp.com',
+      port: 587,
+      domain: 'example.com',
+      user_name: 'adajensen@gmail.com',
+      password: 'x5YqVr7K5JWZ5PQLDkUkTw',
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
